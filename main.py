7e9adcb5a484dc88ac5320ad47e6e3ecc15f2d7f
sametsJR <aliaksandr.samets@gmail.com>
2021-11-08 23:09:47 +0300

    Create 'data' function to collect data from api with json

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import requests# pip install requests in Terminal
 #Создаем функцию, принимающую 2 параметра: город и токен
 def get_weateher(city, open_weather_token):
     try:
+        r = requests.get(
+            f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}"
+        )
+        data = r.json() #получаем данные в формате json и записываем их в переменную data
+        print(data)
+
     except Exception as ex:
         print(ex)
         print("Проверьте название города!!!")#Вывод сообщения об ошибке
